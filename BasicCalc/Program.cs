a5bead6fb92a4b454ed2fefdeb20166e294b5f42
DonKennie <ajeigbekehinde160@gmail>
2021-09-11 22:48:44 +0100

    First commit

---
+++ BasicCalc/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace BasicCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number: ");
+                double result1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter operator: ");
+                string op = Console.ReadLine();
+
+                Console.WriteLine("Enter a number: ");
+                double result2 = double.Parse(Console.ReadLine());
+
+                if (op == "+")
+                {
+                    Console.WriteLine("The answer is: " + result1 + result2);
+                }
+                else if (op == "-")
+                {
+                    Console.WriteLine(result1 - result2);
+
+                }
+                else if (op == "/")
+                {
+                    Console.WriteLine(result1 / result2);
+
+                }
+                else if (op == "*")
+                {
+                    Console.WriteLine("The answer is: " + result1 * result2);
+
+                }
+                else if (op == "%")
+                {
+                    Console.WriteLine(result1 % result2);
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid operator");
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
